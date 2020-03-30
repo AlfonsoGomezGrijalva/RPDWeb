10175d6de6e50115fa483e2efc716bfef9e08615
AlfonsoGomezGrijalva <61067360+AlfonsoGomezGrijalva@users.noreply.github.com>
2020-03-30 16:06:32 -0600

    first commit

---
+++ rpd/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRouters } from './app.routes';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import {DataService} from './data.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+    AppRouters,
+    FlexLayoutModule
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
